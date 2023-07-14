b1775a7bd5bab4b8261de7b1568e6acef59c8b29
Souad Yakubu <121411073+souadyakubu@users.noreply.github.com>
2023-07-14 03:30:47 +0000

    changes

--- feedback_tool.py
+++ feedback_tool.py
@@ -4,21 +4,29 @@ import streamlit as st
 from transformers import BartForConditionalGeneration, BartTokenizer
 
 # establishing connection to the database used
-my_connection = psycopg2.connect(database="postgres", user="souadyakubu", password="souadseny", host="localhost", port="5432")
+database_name = "postgres"
+database_user = "souadyakubu"
+database_password = "souadseny"
+database_host = "localhost"
+database_port = "5432"
+
+my_connection = psycopg2.connect(database = database_name , user = database_user , password = database_password, host = database_host, port = database_port)
 cursor = my_connection.cursor()
 
+
 # making a feedback table 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS feedback (
-        id SERIAL PRIMARY KEY,
-        context TEXT,
-        question TEXT,
-        system_generated TEXT,
-        rater_id INTEGER,
-        rank INTEGER
-    )
-''')
-my_connection.commit()
+with my_connection.cursor() as cursor:
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS feedback (
+            id SERIAL PRIMARY KEY,
+            context TEXT,
+            question TEXT,
+            system_generated TEXT,
+            rater_id INTEGER,
+            rank INTEGER
+        )
+    ''')
+    my_connection.commit()
 
 @st.cache(allow_output_mutation=True)
 def generate_questions(model_choice, context):
@@ -26,14 +34,14 @@ def generate_questions(model_choice, context):
 
 def feedback_log(context, question, system, rater_id, rank):
     # putting the feedback data into the PostgreSQL database
-    cursor.execute(
-        "INSERT INTO feedback (context, question, system_generated, rater_id, rank) VALUES (%s, %s, %s, %s, %s)",
-        (context, question, system, rater_id, rank)
-    )
-    my_connection.commit()
+    with my_connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO feedback (context, question, system_generated, rater_id, rank) VALUES (%s, %s, %s, %s, %s)",
+            (context, question, system, rater_id, rank)
+        )
+        my_connection.commit()
 
-def main():
-    st.title("Feedback Collection Tool")
+st.title("Feedback Collection Tool")
 
     # collecting contexts 
     # contexts = st.text_area("Enter the contexts (one per line):").split("\n")
@@ -45,27 +53,24 @@ def main():
     # random_questions = generate_questions(model_choice, random_context)
 
     # for demonstration purposes, I'll assume we already have a random context and its generated questions.
-    random_context = "This is a sample context."
-    random_questions = ["What is your name?", "What are your hobbies?", "Tell me about your work experience."]
+random_context = "This is a sample context."
+random_questions = ["What is your name?", "What are your hobbies?", "Tell me about your work experience."]
 
-    st.subheader("Context:")
-    st.write(random_context)
+st.subheader("Context:")
+st.write(random_context)
 
-    st.subheader("Generated Questions:")
-    for i, question in enumerate(random_questions):
-        st.write(f"{i + 1}. {question}")
+st.subheader("Generated Questions:")
+for i, question in enumerate(random_questions):
+    st.write(f"{i + 1}. {question}")
         
     #User Feedback
-    selected_question_index = st.selectbox("Select the best question:", [i + 1 for i in range(len(random_questions))])
+selected_question_index = st.selectbox("Select the best question:", [i + 1 for i in range(len(random_questions))])
 
-    rank = st.slider("Rate the question (1 = worst, 5 = best):", 1, 5)
+rank = st.slider("Rate the question (1 = worst, 5 = best):", 1, 5)
 
-    if st.button("Submit Feedback"):
-        selected_question = random_questions[selected_question_index - 1]
-        system_that_generated = "hyechanjun/interview-question-remake"
-        rater_id = 1  #can be changed based on user authentication
-        feedback_log(random_context, selected_question, system_that_generated, rater_id, rank)
-        st.success("Feedback submitted successfully!")
-
-if __name__ == "__main__":
-    main()
+if st.button("Submit Feedback"):
+    selected_question = random_questions[selected_question_index - 1]
+    system_that_generated = "hyechanjun/interview-question-remake"
+    rater_id = 1  #can be changed based on user authentication
+    feedback_log(random_context, selected_question, system_that_generated, rater_id, rank)
+    st.success("Feedback submitted successfully!")
